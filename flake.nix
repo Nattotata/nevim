8de73bfee0bc14dc20550293a2d8cd12d1c3858c
xof <reldies.divad@gmail.com>
2026-05-13 10:43:20 +0200

    chore: add nv shortcut for the binary

--- flake.nix
+++ flake.nix
@@ -7,23 +7,29 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-    in {
+    in
+    {
       packages = forAllSystems (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          mkNevim = profile: 
-            let 
+          mkNevim = profile:
+            let
               cfg = import ./nevim.nix { inherit pkgs profile; };
-            in pkgs.symlinkJoin {
+            in
+            pkgs.symlinkJoin {
               name = "nevim-${profile}";
               paths = cfg.binaries ++ [
                 (pkgs.writeShellScriptBin "nevim" ''
                   export PATH="${pkgs.lib.makeBinPath cfg.binaries}:$PATH"
                   exec ${cfg.nvimPkg}/bin/nvim "$@"
                 '')
+                (pkgs.writeShellScriptBin "nv" ''
+                  exec ${cfg.nvimPkg}/bin/nvim "$@"
+                '')
               ];
             };
-        in {
+        in
+        {
           minimalist = mkNevim "minimalist";
           full = mkNevim "full";
           default = mkNevim "minimalist";
